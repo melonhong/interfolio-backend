b13d3b17d0e144ed05682543d96d37ecf52d5859
melonhong <melonhong46@gmail.com>
2025-07-06 18:23:48 +0900

    feat: CORS 에러 설정

---
+++ src/main/java/com/example/interfolio/config/CorsConfig.java
@@ -0,0 +1,25 @@
+package com.example.interfolio.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**") // 모든 API 경로에 대해
+                        .allowedOrigins("http://localhost:5173") // 프론트 주소
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true); // 필요 시
+            }
+        };
+    }
+}
+
